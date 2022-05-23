238ae7bc473575c0927c3d8812b7909ad52da24b
bilabror <muhammadalhiqny@gmail.com>
2022-05-23 14:31:10 +0700

    change picture in dashboard and fix bugs

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -5,7 +5,7 @@
 <div class="row">
     <div class="col">
         <div class="text-center">
-            <img src="assets/img/logo.jpg" width="400px" class="img-thumbnail rounded mx-auto d-block" alt="...">
+            <img src="assets/img/logokonisltg.png" width="400px" class="rounded mx-auto d-block" alt="...">
         </div>
     </div>
 </div>
